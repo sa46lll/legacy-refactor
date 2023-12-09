080d0badbccaa51cd10f5ca1b582f05b27ce0605
sa46lll <azure394@gmail.com>
2023-12-09 21:06:09 +0900

    test: legacyService 테스트 작성

--- src/test/java/org/sa46lll/LegacyServiceTest.java
+++ src/test/java/org/sa46lll/LegacyServiceTest.java
@@ -1,16 +1,21 @@
 package org.sa46lll;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.sa46lll.domain.Order;
 import org.sa46lll.service.OrderService;
 import org.sa46lll.service.PaymentService;
 import org.sa46lll.service.LegacyService;
@@ -34,7 +39,45 @@ class LegacyServiceTest {
     void orderId가_비어있으면_주문이_되지_않는다(String text) {
         sut.processOrder(text);
 
-        verify(orderService, times(0)).getOrder(anyString());
-        verify(paymentService, times(0)).makePayment(anyDouble());
+        verify(orderService, never()).getOrder(anyString());
+        verify(paymentService, never()).makePayment(anyDouble());
+    }
+
+    @Test
+    void 존재하지_않는_주문을_요청하면_결제가_되지_않는다() {
+        String orderId = "nonExistingOrderId";
+        when(orderService.getOrder(orderId)).thenReturn(null);
+
+        boolean result = sut.processOrder(orderId);
+
+        assertFalse(result);
+        verify(orderService, times(1)).getOrder(orderId);
+        verify(paymentService, never()).makePayment(anyDouble());
+    }
+
+    @Test
+    void 결제가_실패하면_주문이_되지_않는다() {
+        String orderId = "existingOrderId";
+        when(orderService.getOrder(orderId)).thenReturn(new Order(orderId, 1000.0));
+        when(paymentService.makePayment(anyDouble())).thenReturn(false);
+
+        boolean result = sut.processOrder(orderId);
+
+        assertFalse(result);
+        verify(orderService, times(1)).getOrder(anyString());
+        verify(paymentService, times(1)).makePayment(anyDouble());
+    }
+
+    @Test
+    void 결제가_완료되면_주문이_완료된다() {
+        String orderId = "existingOrderId";
+        when(orderService.getOrder(orderId)).thenReturn(new Order(orderId, 1000.0));
+        when(paymentService.makePayment(anyDouble())).thenReturn(true);
+
+        boolean result = sut.processOrder(orderId);
+
+        assertTrue(result);
+        verify(orderService, times(1)).getOrder(anyString());
+        verify(paymentService, times(1)).makePayment(anyDouble());
     }
 }
